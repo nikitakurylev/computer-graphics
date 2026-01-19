15d76764c509fdfe6b6e6712c608c447efb21bf2
Dmitry <Kulikov>
2026-01-19 22:07:34 +0300

    add C# solution and settings for mono

---
+++ Scripting/Scripting/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Scripting
+{
+    public class Class1
+    {
+/*        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static string gimme();*/
+
+        public void Main2()
+        {
+            //System.Console.WriteLine(gimme());
+            System.Console.WriteLine("1");
+/*
+            if (gimme().Equals("All your monos are belong to us!"))
+                return 0;*/
+
+        }
+
+    }
+}
